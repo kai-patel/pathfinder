b7d042234d9447d09e558d726f6aa494f70a7450
Kai Patel <00kaipatel@gmail.com>
2022-09-13 19:28:17 +0100

    Formatting

--- pathfinder.py
+++ pathfinder.py
@@ -2,7 +2,8 @@ import numpy as np
 import pygame
 import sys
 
-class Node():
+
+class Node:
 
     position = None
     neighbours = None
@@ -14,13 +15,18 @@ class Node():
 
     visitable = None
 
-    def __init__(self, i, j, visitable = True):
+    def __init__(self, i, j, visitable=True):
         self.position = (i, j)
         self.neighbours = []
         self.visitable = visitable
 
     def __repr__(self):
-        return "({}, {})".format(self.position[0], self.position[1]) if self.visitable else "######"
+        return (
+            "({}, {})".format(self.position[0], self.position[1])
+            if self.visitable
+            else "######"
+        )
+
 
 MAP_HEIGHT = 8
 MAP_WIDTH = 8
@@ -33,6 +39,7 @@ for i in range(2, len(grid)-2):
         grid[i][j] = 1
 """
 
+
 def printGrid(grid):
     for row in grid:
         print(row)
@@ -41,7 +48,7 @@ def printGrid(grid):
 def genVisitableNeighbours(node):
     node_i, node_j = node.position
     neighbours = []
-    #print(f"Getting neighbours for node: {node.position}")
+    # print(f"Getting neighbours for node: {node.position}")
 
     for i in range(node_i - 1, node_i + 2):
         for j in range(node_j - 1, node_j + 2):
@@ -49,11 +56,13 @@ def genVisitableNeighbours(node):
                 if not (i == node_i and j == node_j):
                     neighbour = grid[i][j]
                     if neighbour.visitable:
-                        #print(f"Neighbour found! {neighbour}")
+                        # print(f"Neighbour found! {neighbour}")
                         neighbours.append(neighbour)
     return neighbours
 
+
 printGrid(grid)
+
 
 def astarsearch(start: Node, end: Node):
     open_list = [start]
@@ -67,14 +76,14 @@ def astarsearch(start: Node, end: Node):
         q.g = 0
 
         neighbours = genVisitableNeighbours(q)
-        
+
         for neighbour in neighbours:
 
             if neighbour.position == end.position:
                 neighbour.parent = q
                 return (True, open_list, closed_list)
 
-            if neighbour not in closed_list: 
+            if neighbour not in closed_list:
                 neighbour_q_distance = 1
                 neighbour.g = q.g + neighbour_q_distance
                 neighbour.h = h(neighbour, end)
@@ -83,8 +92,19 @@ def astarsearch(start: Node, end: Node):
                     open_list.append(neighbour)
                     neighbour.parent = q
                 else:
-                    if len(list(filter(lambda x: x.position == neighbour.position and x.f < neighbour.f, open_list))) == 0:
-                         open_list.append(neighbour)
+                    if (
+                        len(
+                            list(
+                                filter(
+                                    lambda x: x.position == neighbour.position
+                                    and x.f < neighbour.f,
+                                    open_list,
+                                )
+                            )
+                        )
+                        == 0
+                    ):
+                        open_list.append(neighbour)
 
         closed_list.append(q)
 
@@ -94,13 +114,14 @@ def astarsearch(start: Node, end: Node):
 def h(n1, n2):
     return abs(n1.position[0] - n2.position[0]) + abs(n1.position[1] - n2.position[1])
 
+
 def pathfind(firstNode, lastNode):
 
     found, open_list, closed_list = astarsearch(firstNode, lastNode)
     if found:
         print("Path found!")
-        #print(open_list)
-        #print(closed_list)
+        # print(open_list)
+        # print(closed_list)
     else:
         print("No path found")
         return []
@@ -108,12 +129,13 @@ def pathfind(firstNode, lastNode):
     path = []
 
     while lastNode and lastNode.position not in path:
-        #print(lastNode.parent.position)
+        # print(lastNode.parent.position)
         path.append(lastNode.position)
         lastNode = lastNode.parent
-        #print(path)
-    
+        # print(path)
+
     return reversed(path)
+
 
 def main():
 
@@ -121,13 +143,11 @@ def main():
     size = WINDOW_W, WINDOW_H = 512, 512
     CELL_W = WINDOW_W / MAP_WIDTH
     CELL_H = WINDOW_H / MAP_HEIGHT
-    
+
     screen = pygame.display.set_mode(size)
     surface = pygame.display.get_surface()
 
     editing = True
-    
-    
 
     while editing:
         for event in pygame.event.get():
@@ -135,8 +155,8 @@ def main():
                 sys.exit(0)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouseW, mouseH = pygame.mouse.get_pos()
-                mouseW = int(mouseW//CELL_W)
-                mouseH = int(mouseH//CELL_H)
+                mouseW = int(mouseW // CELL_W)
+                mouseH = int(mouseH // CELL_H)
                 grid[mouseW][mouseH] = 1 if grid[mouseW][mouseH] == 0 else 0
                 print(mouseW, mouseH)
             elif event.type == pygame.KEYDOWN:
@@ -149,11 +169,15 @@ def main():
             for j in range(MAP_WIDTH):
                 width = 1 if grid[i][j] == 0 else 0
                 color = (0, 255, 0) if grid[i][j] == 0 else (255, 0, 0)
-                pygame.draw.rect(surface, color, pygame.Rect(i*CELL_W, j*CELL_H, CELL_W, CELL_H), width)
-        
-        #pygame.draw.rect(surface, (0, 255, 255), pygame.Rect(0, 0, CELL_W, CELL_H))
-        pygame.display.update()
+                pygame.draw.rect(
+                    surface,
+                    color,
+                    pygame.Rect(i * CELL_W, j * CELL_H, CELL_W, CELL_H),
+                    width,
+                )
 
+        # pygame.draw.rect(surface, (0, 255, 255), pygame.Rect(0, 0, CELL_W, CELL_H))
+        pygame.display.update()
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -168,7 +192,7 @@ def main():
         grid[r][c] = "****"
 
     printGrid(grid)
-    print(", ".join(map(str,path)))
+    print(", ".join(map(str, path)))
 
     while 1:
         for event in pygame.event.get():
@@ -189,8 +213,13 @@ def main():
                     if not cell.visitable:
                         width = 0
                         color = (255, 0, 0)
-                
-                pygame.draw.rect(surface, color, pygame.Rect(i*CELL_W, j*CELL_H, CELL_W, CELL_H), width)
+
+                pygame.draw.rect(
+                    surface,
+                    color,
+                    pygame.Rect(i * CELL_W, j * CELL_H, CELL_W, CELL_H),
+                    width,
+                )
 
         pygame.display.update()
 
